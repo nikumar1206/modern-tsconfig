e78c51846dea2cfa4a7b61f5af431528c288accf
Nikhil Kumar <nikumar1206@gmail.com>
2022-10-07 19:48:32 -0400

    debugging with console.log

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
 #! /usr/bin/env node
-// goal: will write a tsconfig.json to your root directory
-
 import * as path from "path";
 import { promises as fs } from "fs";
-
 const __dirname = path.resolve();
 const pathToConfig = path.join(__dirname, "tsconfig.react.json");
 console.log(pathToConfig);
-
 const files = await fs.readdir(__dirname);
 console.log(__dirname);
 console.log(files);
-
 const main = async () => {
-  try {
-    const data = await fs.readFile(pathToConfig, "utf8");
-    await fs.writeFile("tsconfig.json", data);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const data = await fs.readFile(pathToConfig, "utf8");
+        await fs.writeFile("tsconfig.json", data);
+    }
+    catch (error) {
+        console.log(error);
+    }
 };
-
 try {
-  await main();
-} catch (error) {
-  console.log(error);
+    await main();
 }
+catch (error) {
+    console.log(error);
+}
+//# sourceMappingURL=index.js.map
