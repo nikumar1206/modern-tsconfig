d0b8ec2c59d3246a0b4a9e2a5bd010a33ac4c1ea
Nikhil Kumar <nikumar1206@gmail.com>
2022-10-07 19:19:56 -0400

    first attempt at running a default tsconfig on project folder

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+// goal: will write a tsconfig.json to your root directory
+
+import * as path from "path";
+import { promises as fs } from "fs";
+
+const __dirname = path.resolve();
+const pathToConfig = path.join(__dirname, "dist", "tsconfig.react.json");
+console.log(pathToConfig);
+
+const main = async () => {
+  try {
+    const data = await fs.readFile(pathToConfig, "utf8");
+    await fs.writeFile("tsconfig.json", data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+try {
+  await main();
+} catch (error) {
+  console.log(error);
+}
