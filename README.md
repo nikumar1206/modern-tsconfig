91e62a730c098394c9451097ea069dbf19946348
Nikhil Kumar <nikumar1206@gmail.com>
2022-10-16 18:54:08 -0400

    more robust tsconfigs (stricter rules), added support for node, better README, added inquirer for better cli experience

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # modern-tsconfigs
 
-Use an opinionated tsconfig for faster TypeScript project setup. Inspired by Ben Awad.
+Use an opinionated tsconfig for faster TypeScript project setup.
+Supports React and Node configs only.
+
+Inspired by Ben Awad.
+
+## How To Install
+
+For NPM:
+
+```
+npx modern-tsconfig
+```
+
+For PNPM:
+
+```
+pn dlx modern-tsconfig
+```
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,23 @@
 // goal: will write a tsconfig.json to your root directory
 
 import * as path from "path";
+import inquirer from "inquirer";
 import { promises as fs } from "fs";
 
-const config = path.resolve(__dirname, "tsconfig.react.json");
-
 const main = async () => {
+	const { selectedTech } = await inquirer.prompt([
+		{
+			type: "list",
+			message: "Pick the technology you're using:",
+			name: "selectedTech",
+			choices: ["react", "node"],
+		},
+	]);
+
+	const configPath = path.resolve(__dirname, `tsconfig.${selectedTech}.json`);
+
 	try {
-		const data = await fs.readFile(config, "utf8");
+		const data = await fs.readFile(configPath, "utf8");
 		const configLocation = path.join(process.cwd(), "tsconfig.json");
 		await fs.writeFile(configLocation, data);
 	} catch (error) {
